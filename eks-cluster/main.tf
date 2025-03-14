49072f439bfedc6bfc490d6ad618869c67a27b8b
Edmore Mlotshwa <mlotshwa.edmore@gmail.com>
2025-03-14 15:10:18 +0200

    Uncomment creds &filter out comments

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -1,10 +1,7 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
   region = var.region
-  #access_key = AWS_ACCESS_KEY_ID
-  #secret_key = AWS_SECRET_ACCESS_KEY
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
 }
 
 # Filter out local zones, which are not currently supported 
